d8a686b1c061edf26edb14119f4dc7c68b7bdf68
rkojantaev <kojantaev2992@gmail.com>
2022-12-29 08:00:51 +0600

    article habr

--- src/main/java/com/rahatbek/mytest/multithreads/arcticlehabr/post164487/incremenator/Program.java
+++ src/main/java/com/rahatbek/mytest/multithreads/arcticlehabr/post164487/incremenator/Program.java
@@ -5,11 +5,13 @@ class Incremenator extends Thread {
     private volatile boolean mIsIncrement = true;
     private volatile boolean mFinish = false;
 
-    public void changeAction() { //Меняет действие на противоположное
+    public void changeAction() {   //Меняет действие на противоположное
+
         mIsIncrement = !mIsIncrement;
     }
 
-    public void finish() { //Инициирует завершение потока
+    public void finish()        //Инициирует завершение потока
+    {
         mFinish = true;
     }
 
@@ -17,7 +19,6 @@ class Incremenator extends Thread {
     public void run() {
         do {
             if (!mFinish) {   //Проверка на необходимость завершения
-
                 if (mIsIncrement)
                     Program.mValue++;    //Инкремент
                 else
@@ -38,24 +39,29 @@ class Incremenator extends Thread {
 }
 
 public class Program {
-    // Переменная, которой оперирует инкременатор
+    //Переменая, которой оперирует инкременатор
     public static int mValue = 0;
 
-    static Incremenator mInc;	//Объект побочного потока
+    static Incremenator mInc;    //Объект побочного потока
 
     public static void main(String[] args) {
-        mInc = new Incremenator(); // Создание потока
+        mInc = new Incremenator();    //Создание потока
 
         System.out.print("Значение = ");
 
-        mInc.start();
+        mInc.start();    //Запуск потока
 
         //Троекратное изменение действия инкременатора
         //с интервалом в i*2 секунд
         for (int i = 1; i <= 3; i++) {
             try {
-                Thread.sleep(i * 2 * 1000);
-            } catch (InterruptedException e) {}
+                Thread.sleep(i * 2 * 1000); //Ожидание в течении i*2 сек.
+            } catch (InterruptedException e) {
+            }
+
+            mInc.changeAction();    //Переключение действия
         }
+
+        mInc.finish();    //Инициация завершения побочного потока
     }
 }
