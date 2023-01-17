d7d2850209c786fd40f0be6f2e2dba5804f42929
rkojantaev <kojantaev2992@gmail.com>
2023-01-17 08:57:26 +0600

    on second part of multithreading A.V

---
+++ src/main/java/com/rahatbek/mytest/multithreads/alekseivladykin/demo11/SequenceGeneratorBroken.java
@@ -0,0 +1,28 @@
+package com.rahatbek.mytest.multithreads.alekseivladykin.demo11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SequenceGeneratorBroken {
+
+    private static volatile int counter = 0;
+
+    public static int nextInt() {
+        return counter++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 1000; ++j) nextInt();});
+            thread.start();
+            threads.add(thread);
+            }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println("Counter final value: " + counter);
+    }
+}
---
+++ src/main/java/com/rahatbek/mytest/multithreads/alekseivladykin/demo11/SequenceGeneratorGood2.java
@@ -0,0 +1,29 @@
+package com.rahatbek.mytest.multithreads.alekseivladykin.demo11;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SequenceGeneratorGood2 {
+
+    private static final AtomicInteger counter = new AtomicInteger();
+
+    public static int nextInt() {
+        return counter.getAndIncrement();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 1000; ++j) nextInt();});
+            thread.start();
+            threads.add(thread);
+            }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println("Counter final value: " + counter);
+    }
+}
